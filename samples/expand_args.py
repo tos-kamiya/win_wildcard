e49ffcd4cafbc858d16275688ab3199237d7d0ce
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2022-09-11 02:20:45 +0900

    fix: update sample script

--- samples/expand_args.py
+++ samples/expand_args.py
@@ -5,6 +5,10 @@ from win_wildcard import expand_windows_wildcard
 files = []
 for a in sys.argv[1:]:
     r = expand_windows_wildcard(a)
+    if not r:
+        print("warning: no matching files for the wildcard: %s" % a)
+        continue
     files.extend(r)
 
-print('\n'.join(files))
+if files:
+    print('\n'.join(files))
